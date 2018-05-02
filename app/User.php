8d59367e4c524beffe6cf7617a3f5a5ae4c63477
HereIam <401823611@qq.com>
2018-05-02 18:06:02 +0800

    第一次提交

---
+++ app/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
+
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use EntrustUserTrait;//权限管理
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function  add(){
+        $user = User::where('username', '=', 'michele')->first();
+
+    // 或者也可以使用Eloquent原生的方法
+        $user->roles()->attach(1); //只需传递id即可
+    }
+}
